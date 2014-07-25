cacc2fa7604b84f2585471ff906e9273b6687376
Andre <aventura@sato7.com.br>
2014-07-25 15:29:52 -0300

    tarefa 2440, outra 2324

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -2,7 +2,7 @@ class NewsController < ApplicationController
   before_filter :load_event_notices
   
   def index
-    @news = Notice.all(:order => "id DESC")
+    @news = Notice.all(:order => "created_at DESC")
   end
   
   def show
